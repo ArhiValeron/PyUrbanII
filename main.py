629fbd5cae905f64ae39359fa8e7e34918a31902
ArhiValeron <burdin.vv@gmail.com>
2024-07-28 18:44:12 +0500

    1

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
         return
+
     inner_function()
     return
+
 
 try:
     inner_function()
